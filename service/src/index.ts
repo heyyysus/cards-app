e7879698e91c345036fce0e1d8213e8517ae2d69
Jesus Velarde <jesus.velade07@gmail.com>
2022-10-28 14:02:54 -0700

    created api description at GET /api

--- service/src/index.ts
+++ service/src/index.ts
@@ -4,8 +4,22 @@ const PORT = 5000;
 
 const app = Express();
 
-app.get('/', (req, res) => {
-    res.send("HELLO WORLD N SHIT");
+interface ApiDescription {
+    name: string,
+    version: string,
+    description: string,
+    routes: {url: string, credentials: string}[]
+}
+
+const apiDescription: ApiDescription = {
+    name: "gaucho_group_service",
+    version: "v0.1",
+    description: "backend for gaucho group application.",
+    routes: [],
+};
+
+app.get('/api', (req, res) => {
+    res.json(apiDescription);
 })
 
 app.listen(PORT, () => {
