74dab69724905d9d6cc74671637f1f779adfbbcb
Jesus Velarde <jev@ucsb.edu>
2023-02-23 08:00:22 -0800

    dev docker-compose created

--- db/init.sql
+++ db/init.sql
@@ -1,11 +1,12 @@
-CREATE TYPE credential_services AS ENUM ('google', 'apple');
+CREATE TYPE auth_role AS ENUM ('USER', 'ADMIN');
 
 CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(30) NOT NULL,
     email VARCHAR(30) NOT NULL,
     profile_img VARCHAR(255),
-    credentials_provider credential_services NOT NULL
+    auth auth_role DEFAULT 'USER',
+    ts TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS courses (
@@ -13,7 +14,8 @@ CREATE TABLE IF NOT EXISTS courses (
     course_subject VARCHAR(255),
     course_no VARCHAR(10),
     course_name VARCHAR(255) NOT NULL,
-    instructor_name VARCHAR(255) NOT NULL
+    instructor_name VARCHAR(255) NOT NULL,
+    ts TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS groups (
@@ -21,5 +23,6 @@ CREATE TABLE IF NOT EXISTS groups (
     group_name VARCHAR(30) NOT NULL,
     group_img VARCHAR(255),
     description VARCHAR(500),
-    course_id INT REFERENCES courses(course_id)
+    course_id INT REFERENCES courses(course_id),
+    ts TIMESTAMP DEFAULT NOW()
 );
