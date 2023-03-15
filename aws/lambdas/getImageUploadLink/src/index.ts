9790def021c773a5228df28c575895601757f07c
Jesus Velarde <jev@ucsb.edu>
2023-03-15 15:00:03 -0700

    getImageUploadUrl

--- aws/lambdas/getImageUploadLink/src/index.ts
+++ aws/lambdas/getImageUploadLink/src/index.ts
@@ -1,22 +1,59 @@
 import { APIGatewayProxyResult } from "aws-lambda";
 import { APIEvent, RequestContext } from "./types";
-import AWS from 'aws-sdk';
-import multipart from 'aws-lambda-multipart-parser';
+import { S3 } from "aws-sdk";
 import config from './config';
 
-const s3 = new AWS.S3({
-    accessKeyId:config?.dev?.aws_s3?.ACCESS_KEY,
-    secretAccessKey:config?.dev?.aws_s3?.SECRET_KEY
-  })
+class InvalidExtensionError extends Error {};
 
+const isValidExtension = (ext: string) => {
+    if(!ext) return false;
+    const l_ext = ext.toLowerCase();
+    if(l_ext === 'png' || l_ext === 'jpg' || l_ext === 'jpeg')
+        return true;
+    return false;
+}
+
+const createPresignedImageUploadUrl = async (user_id: string, extension: string) => {
+    const s3 = new S3();
+    const region = "us-west-2";
+    const bucket = "hoppin-app-static";
+    const filename = `${user_id}.${extension}`;
+    const key = `images/profile/${filename}`;
+    const presignedUrl = await s3.getSignedUrlPromise('putObject', {
+        Bucket: bucket,
+        Key: key,
+        Expires: 100,
+        ContentType: `image/${extension}`
+    });
+
+    return { url: presignedUrl, filename: filename }
+    
+  };
+  
 
 export const handler = async (event: APIEvent, context: RequestContext): Promise<APIGatewayProxyResult> => {
     try {
-        if(event.requestContext.http.method !== "PATCH"){
-            
-        } 
+        const method = event.requestContext.http.method;
+        const sub = event.requestContext.authorizer.jwt.claims.sub;
+        switch(method){
+        case "GET":
+            const extension = event.queryStringParameters?.extension || undefined;
+            if(!isValidExtension(extension)) throw new InvalidExtensionError;
+
+            const signedUrl = await createPresignedImageUploadUrl(sub, extension);
+            console.log(signedUrl);
+            return {
+                statusCode: 200,
+                body: JSON.stringify(signedUrl)
+            };
+        }
     } catch(e){
         console.log(e);
+        if(e instanceof InvalidExtensionError)
+            return {
+                statusCode: 400,
+                body: "Invalid Extension Type"
+            };
         return { 
             statusCode: 500,
             body: "Internal Error"
