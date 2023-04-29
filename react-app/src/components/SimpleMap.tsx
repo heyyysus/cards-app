3feb7158bf7b1c0ef36dcc60ac17dcea2ff028e7
Jesus Velarde <jev@ucsb.edu>
2023-04-28 19:53:27 -0700

    add Google Maps API key

--- react-app/src/components/SimpleMap.tsx
+++ react-app/src/components/SimpleMap.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import GoogleMapReact from 'google-map-react';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { GOOGLE_MAPS_API_KEY}  from "../secret.json";
 
 const AnyReactComponent = ({ text, lat, lng }: {text: string, lat: number, lng: number}) => 
     <LocationOnIcon sx={{width: '5vh', height: '5vh', position: 'relative', right: '2.5vh', bottom: '4vh'}} color='primary' />;
@@ -20,7 +21,7 @@ export const  SimpleMap: FC<SimpleMapProps> = ({ center, zoom }) => {
     // Important! Always set the container height explicitly
     <div style={{ height: '50vh', width: '100%' }}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
+        bootstrapURLKeys={{ key: GOOGLE_MAPS_API_KEY }}
         defaultCenter={center}
         defaultZoom={zoom}
       >
