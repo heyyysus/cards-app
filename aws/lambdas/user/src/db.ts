156826a6884c17a41cbef1a0615574d7c6e22653
Jesus Velarde <jev@ucsb.edu>
2023-05-08 15:06:57 -0700

    add db password to secret file

--- aws/lambdas/user/src/db.ts
+++ aws/lambdas/user/src/db.ts
@@ -1,10 +1,11 @@
 import { Pool } from 'pg';
+import { DB_PASSWORD } from "./secret";
 
 const pool = new Pool({
     host: "group-study-ucsb-dev.c8nxscgmv2nn.us-west-2.rds.amazonaws.com",
     port: 5432,
     user: "postgres",
-    password: "RPloa9Wa04gkcmtHFwWFl",
+    password: DB_PASSWORD,
 });
 
 export default pool;
